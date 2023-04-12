6e1bb633cc30baa671292d4ef27ebd5ab9642fe9
Richard Hernandez <rhernandez@meatcode.cl>
2023-04-11 23:04:50 -0400

    test

--- src/plugins/contacts/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin contacts
-
-A quick description of contacts.
--- src/plugins/contacts/admin/src/api/contact.js
+++
@@ -1,9 +0,0 @@
-import { request } from '@strapi/helper-plugin';
-
-const contactsRequest = {
-  getAllContacts: async () => {
-    return await request('/contacts', { method: 'GET' });
-  }
-}
-
-export default contactsRequest;
