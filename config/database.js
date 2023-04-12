07814197fcbbe959cd3729c0d7a137f7d3964236
Richard Hernandez <rhernandez@meatcode.cl>
2023-04-11 22:53:08 -0400

    add  test

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
@@ -8,6 +8,19 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', 'strapi'),
       ssl: env.bool(false),
+    },
+  },
+}); */
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
     },
   },
 });
